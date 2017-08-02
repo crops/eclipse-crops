1d4246ccd977b1ccfdeb09b5b42fa6cae7889187
Chin Huat Ang <chin.huat.ang@intel.com>
2017-08-02 16:07:37 +0800

    Add Makefile project plugins

---
+++ toolchains/sdk/org.yocto.crops.make/src/org/yocto/crops/make/MakeNature.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Intel, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.yocto.crops.make;
+
+import org.eclipse.cdt.core.build.CBuilder;
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.yocto.crops.make.Activator;
+
+public class MakeNature implements IProjectNature {
+
+	public static final String ID = Activator.getId() + ".makeNature"; //$NON-NLS-1$
+
+	private IProject project;
+
+	public static void setupBuilder(IProjectDescription projDesc) throws CoreException {
+		ICommand command = projDesc.newCommand();
+		CBuilder.setupBuilder(command);
+		projDesc.setBuildSpec(new ICommand[] { command });
+	}
+	
+	@Override
+	public void configure() throws CoreException {
+		IProjectDescription projDesc = project.getDescription();
+		setupBuilder(projDesc);
+		project.setDescription(projDesc, new NullProgressMonitor());
+	}
+
+	@Override
+	public void deconfigure() throws CoreException {
+	}
+
+	@Override
+	public IProject getProject() {
+		return project;
+	}
+
+	@Override
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+
+}
---
+++ toolchains/sdk/org.yocto.crops.make/templates/make/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	printf("Hello from Makefile!\n");
+	return 0;
+}
